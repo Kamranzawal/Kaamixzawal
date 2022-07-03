2f734614dee830f3055d3a9f411c2c82244a9a76
Kamranzawal <105263028+Kamranzawal@users.noreply.github.com>
2022-07-04 00:45:54 +0500

    Update Kaamixzawal.py

--- Kaamixzawal.py
+++ Kaamixzawal.py
@@ -9,7 +9,6 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
  
-    from Kaamix import Subscription
-    Subscription()
+    from Kaamix import
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
