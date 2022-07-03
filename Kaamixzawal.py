cd2a0549c319e76d4b47fa5bfa0a43bb19187f87
Kamranzawal <105263028+Kamranzawal@users.noreply.github.com>
2022-07-03 11:41:53 +0500

    Update Kaamixzawal.py

--- Kaamixzawal.py
+++ Kaamixzawal.py
@@ -1,2 +1,15 @@
-print("\033[:32m     Welcome To The My Tool")
-import Kaamix
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
+ 
+    from Kaamix import Subscription
+    Subscription()
+elif bit == '32bit':
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
