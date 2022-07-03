af5ba37489a862309f7356c07f598392b07ce9b7
Kamranzawal <105263028+Kamranzawal@users.noreply.github.com>
2022-07-04 01:32:09 +0500

    Update Kaamixzawal.py

--- Kaamixzawal.py
+++ Kaamixzawal.py
@@ -1,14 +1,14 @@
-import os, platform, time
+import os, platform
 try:
     import requests
 except:
     os.system('pip install requests')
 os.system('git pull')
 import requests
+ 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
- 
-    from Kaamix import
+    import Kaamix
+    
 elif bit == '32bit':
-    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
+    import Kaamix
